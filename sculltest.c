557dc1db1e1bac96af02f6367be46636d7578903
iamdharm20 <iamdharm20@gmail.com>
2015-04-09 11:43:16 +0530

    Update sculltest.c

--- sculltest.c
+++ sculltest.c
@@ -3,73 +3,49 @@
 * operations of the "/dev/scull" device (a.k.a "scull0"),
 * and the 
 */
- 
+
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
 #include <fcntl.h>
 
-int main() 
+int main()
 {
-	int fd, result, len;
-	char buf[10];
-	const char *str;
-	if ((fd = open ("/dev/scull", O_WRONLY)) == -1) 
-	{
-		perror("open failed");
-		return -1;
-	}
-	str = "abcde"; 
-	len = strlen(str);
-	if ((result = write (fd, str, len)) != len)
-	{
-		perror("write failed");
-		return -1;
-	}
-	close(fd);
-	if ((result = read (fd, &buf, sizeof(buf))) != len) 
-	{
-		perror("read failed");
-		return -1;
-	} 
-	buf[result] = '\0';
-	if (strncmp (buf, str, len)) 
-	{
-		fprintf (stdout, "failed: read back \"%s\"\n", buf);
-	}
-	else 
-	{
-		fprintf (stdout, "passed\n");
-	}
-	close(fd);
-	
-/*	str = "xyz"; 
-	len = strlen(str);
-	if ((fd = open ("/dev/scullpipe", O_RDWR)) == -1) 
-	{
-		perror("open failed");
-		return -1;
-	}
-	if ((result = write (fd, str, len)) != len) 
-	{
-		perror("write failed");
-		return -1;
-	}
-	if ((result = read (fd, &buf, sizeof(buf))) != len) 
-	{
-		perror("read failed");
-		return -1;
-	}
-	buf[result] = '\0';
-	if (strncmp (buf, str, len)) 
-	{
-		fprintf (stdout, "failed: read back \"%s\"\n", buf);
-	} 
-	else 
-	{
-		fprintf (stdout, "passed\n");
-	}
-	close(fd);	*/
-	return 0;
+    int fd, result, len;
+    char buf[10];
+    const char *str;
+    if ((fd = open ("/dev/scull", O_WRONLY)) == -1)
+    {
+        perror("open failed");
+        return -1;
+    }
+    printf("fd = %d\n",fd);
+    str = "abcde";
+    len = strlen(str);
+    printf("len = %d\n",len);
+    if ((result = write (fd, str, len)) != len)
+    {
+        perror("write failed");
+        return -1;
+    }
+    printf("written bytes are = %d\n",result);
+    close(fd);
+    if ((result = read (fd, &buf, sizeof(buf))) != len)
+    {
+        perror("read failed");
+        return -1;
+    }
+    printf("read bytes are = %d\n",result);
+    buf[result] = '\0';
+    if (strncmp (buf, str, len))
+    { 
+        fprintf (stdout, "failed: read back \"%s\"\n", buf);
+    }
+    else
+    {
+        fprintf (stdout, "passed\n");
+    } 
+    close(fd);
+      
+    return 0;
 }
-
