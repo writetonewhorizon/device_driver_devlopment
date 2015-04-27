95fdfe39495e5391babed0a61e489908af82e4be
dharmender suresh chander <dharmender.sureshchander@in.bosch.com>
2015-04-27 09:22:25 +0530

    testing
    Signed-off-by: dharmender suresh chander <dharmender.sureshchander@in.bosch.com>

--- scull_load.sh
+++ scull_load.sh
@@ -9,11 +9,11 @@ sudo mknod /dev/scull1 c 250 1
 sudo mknod /dev/scull2 c 250 2
 sudo mknod /dev/scull3 c 250 3
 sudo ln -sf scull0 /dev/scull
-sudo chgrp dus5cob /dev/scull[0-3]
+sudo chgrp dharmender /dev/scull[0-3]
 sudo chmod 644 /dev/scull[0-3]
 ls -al /dev/ | grep scull
 sudo ./sculltest
-dmesg
-sudo rmmod scull
-dmesg
-//sudo modprobe -r scull or rmmod --help
+#dmesg
+#sudo rmmod scull
+#dmesg
+#sudo modprobe -r scull or rmmod --help
